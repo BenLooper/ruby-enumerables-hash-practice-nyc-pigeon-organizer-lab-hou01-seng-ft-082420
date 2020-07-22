f1c3bd58c0a27957dc3006cedbe297b544a98512
Benjamin Arthur Looper <benlooper08@gmail.com>
2020-07-22 00:50:40 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,6 +1,7 @@
 require 'pry'
 def nyc_pigeon_organizer(data)
   results = data.each_with_object({}) do |(key,value), final_NDS|
-    binding.pry
+    value.each do |names| 
+      binding.pry
 end
 end 
